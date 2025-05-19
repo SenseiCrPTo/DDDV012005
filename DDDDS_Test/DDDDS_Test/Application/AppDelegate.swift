c8adf3c96f362af85b1358efa9b4b08ac1d507ff
SenseiCrPTo <insidearcheolog@gmail.com>
2025-05-19 15:03:04 +0800

    Delete DDDDS_Test directory

--- DDDDS_Test/DDDDS_Test/Application/AppDelegate.swift
+++
@@ -1,7 +0,0 @@
-// AppDelegate.swift
-func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = DashboardViewController() // ← Вот это
-    window?.makeKeyAndVisible()
-    return true
-}
--- DDDDS_Test/DDDDS_Test/Extensions/UIView+SwiftUI.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UIView+SwiftUI.swift
-//  DDDDS_Test
-//
-//  Created by Vladimir Svh on 19/05/2025.
-//
-
-import Foundation
