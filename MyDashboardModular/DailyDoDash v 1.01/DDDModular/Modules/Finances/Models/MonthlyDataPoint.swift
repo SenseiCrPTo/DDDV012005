4795983412bc220e19a026a7c2e09846b5b597b7
SenseiCrPTo <insidearcheolog@gmail.com>
2025-05-19 09:21:25 +0800

    Delete MyDashboardModular/DailyDoDash v 1.01 directory

--- MyDashboardModular/DailyDoDash v 1.01/DDDModular/Modules/Finances/Models/MonthlyDataPoint.swift
+++
@@ -1,9 +0,0 @@
-import Foundation // SwiftUI здесь не нужен
-
-struct MonthlyDataPoint: Identifiable {
-    let id = UUID()
-    let month: String
-    let date: Date
-    let value: Double
-    var type: String  // "Доход", "Расход", "Накопления"
-}
--- MyDashboardModular/DailyDoDash v 1.01/DDDModular/Shared/Utils/SequenceExtension.swift
+++
@@ -1,9 +0,0 @@
-import Foundation // Для Set
-
-// MARK: - Утилиты для Последовательностей
-extension Sequence where Element: Hashable {
-    var unique: [Element] {
-        var set = Set<Element>()
-        return filter { set.insert($0).inserted }
-    }
-}
